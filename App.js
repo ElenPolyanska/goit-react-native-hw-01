05cf90e91f7f483674d27f2fb50730231c3e24c0
ElenPolyanska <evp387@gmail.com>
2023-06-21 11:12:35 +0300

    hw01

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO WORLD!</Text>
+      <Text>ПРИВІТ СВІТ!</Text>
       <StatusBar style="auto" />
     </View>
   );
